026ad83ee9f3ba7b65571b8547e647207d7e42fc
Pius Udoma <udomahpius5@gmail.com>
2023-08-07 02:51:30 +0100

    init

--- js/main.js
+++ js/main.js
@@ -106,3 +106,33 @@ const navScrollfixed = window.addEventListener('scroll', () => {
             })
    
        
+
+// appointment section display hidden content
+            let scriptFunction = false;
+            let EleHidden = document.querySelector('.appointment-hidden');
+            EleHidden.classList.add('css-appointment-hidden');
+           let btnInnerText = document.querySelector('.btn-display-appointment');
+             
+
+let buttomDisplayHiddenContent = document.querySelector('.bottom-btn-display-appointment').addEventListener('click', () => {
+    EleHidden.classList.add('css-appointment-hidden');
+    btnInnerText.innerHTML = "View ALL Appointments";
+           })
+            let btnEle = document.querySelector('.btn-display-appointment').addEventListener('click', () => {
+                
+                if (scriptFunction === false) {
+                    EleHidden.classList.remove('css-appointment-hidden');
+                    btnInnerText.innerHTML = "Hide Content";
+                    scriptFunction = true;
+
+                } else if (scriptFunction === false) {
+                    buttomDisplayHiddenContent();
+                    scriptFunction = true;
+                }
+                else {
+                    EleHidden.classList.add('css-appointment-hidden');
+                    btnInnerText.innerHTML = "View ALL Appointments";
+                    scriptFunction = false;
+
+                }
+            })
